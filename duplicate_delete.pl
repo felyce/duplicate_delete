22da6e6ad4a38579a62ae01e080554f0b05a0e73
Dai Takahashi <dai@yamane.me>
2012-10-19 22:46:19 +0900

    complete, maybe

--- duplicate_delete.pl
+++ duplicate_delete.pl
@@ -8,7 +8,7 @@ use File::Spec;
 use File::Copy qw/move/;
 use File::Basename;
 
-use Digest::MD5 qw(md5);
+use Digest::MD5; # qw(md5 md5_hex);
 
 my $trash = "trash";
 
@@ -25,67 +25,83 @@ $root_path = $ARGV[0];
 
 find(\&walk, $root_path);
 
-# 重複ファイルの選り分け
-foreach my $file (@file_list) {
-    #print $file, "\n";
-    my $md5hash = make_md5($file);
-    my $abs_path = File::Spec->rel2abs($file);
-
-    if ($digest_list{$md5hash} eq ""){
-        # 重複なし
-        $digest_list{$md5hash} = $abs_path;
-    }
-    else {
-        # 重複あり
-        if (length($digest_list{$md5hash}) < $abs_path) {
-            push(@del_list, $digest_list{$md5hash});
-            $digest_list{$md5hash} = $abs_path;
-        }
-        else {
-            push(@del_list, $abs_path);
-        }
-        $file_count += 1;
-    }
-    #push(@digest_list, make_md5($file));
-}
+choice_files();
 
 mk_trash();
+move_to_trash();
 
-foreach my $del_file (@del_list) {
-    my $result;
-    my $count;
-    $result = move $del_file, $trash.basename($del_file);
-    if ($result) {
-        print "$del_file is removed.";
-        $count += 1;
-    }
-    else {
-        print "Can not remove $del_file:$!";
-    }
-
-    print "Delete $count files.";
-}
-
-print "End";
+print "End\n";
 
 ########################################
 
 sub walk {
-    #print $File::Find::name;
+    #print $File::Find::name."\n";
 
     push(@file_list, $File::Find::name);
 }
 
+
+# 重複ファイルの選り分け
+sub choice_files {
+    foreach my $file (@file_list) {
+        my $abs_path = File::Spec->rel2abs($file);
+        my $md5hash = make_md5($abs_path);
+        next unless defined($md5hash);
+
+        # 重複なし        
+        unless ( defined($digest_list{$md5hash}) ) {
+            $digest_list{$md5hash} = $abs_path;
+        }
+        else {
+            # 重複あり
+            if (length($digest_list{$md5hash}) < length($abs_path)) {
+                push(@del_list, $digest_list{$md5hash});
+                $digest_list{$md5hash} = $abs_path;
+            }
+            else { push(@del_list, $abs_path); }
+
+            $file_count += 1;
+        }
+        #push(@digest_list, make_md5($file));
+    }
+}
+
 sub make_md5 {
-    my $file = shift;
+    my $file_name = shift;
+    if (-d $file_name) {
+        return undef;
+    }
+    open ( my $fh, '<', $file_name ) or die "Can not open $file_name:$!";
+    binmode($fh);
 
-    my $digest = md5($file);
-
-    $digest;
+    my $md5 = Digest::MD5->new;#->addfile($fh)->hexdigest;
+    $md5->addfile($fh);
+     
+    return $md5->hexdigest;
 }
 
 sub mk_trash {
-    mkdir "$trash"
-        or die "$trashの作成に失敗しました。:.$!";
+    if ( -d $trash ) { print "trash directory ... ok.\n"; }
+    else {
+        mkdir "$trash"
+            or die "$trashの作成に失敗しました。:$!";
+    }
     
+}
+
+sub move_to_trash {
+    my $count;
+
+    foreach my $del_file (@del_list) {
+        my $result;
+
+        $result = move $del_file, File::Spec->catfile($trash,basename($del_file));
+        if ($result) {
+            print "$del_file is removed.\n";
+            $count += 1;
+        }
+        else { print "Can not remove $del_file:$!\n"; }
+
+    }
+    print "Delete $count files.\n";
 }
